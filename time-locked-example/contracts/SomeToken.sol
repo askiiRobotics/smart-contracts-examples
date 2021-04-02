40fe6466bcbb5793f4b40974b3b3d311e365bbdc
askiiRobotics <askii.robotics@gmail.com>
2021-04-03 01:56:46 +0300

    Update SomeToken.sol
    unification of the naming

--- time-locked-example/contracts/SomeToken.sol
+++ time-locked-example/contracts/SomeToken.sol
@@ -5,7 +5,7 @@ pragma solidity >=0.6.0 <0.8.0;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract SomeToken is ERC20 {
-    constructor() ERC20('Paypolitan Token', 'Some') public {
+    constructor() ERC20('Some Token', 'Some') public {
         _setupDecimals(18);
         _mint(msg.sender, 94697000000000000000000000);  
     }
